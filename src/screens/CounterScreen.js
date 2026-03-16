26970b86e2d49771c8ddbaac44f1f326aae41e7c
Alex Kutzke <alexkutzke@gmail.com>
2026-03-16 08:42:42 -0300

    feat: adicionar um contador simples

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Counter from '../components/Counter';
+
+const CounterScreen = () => {
+  return (
+    <View>
+      <Counter />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default CounterScreen;
